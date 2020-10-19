b09f41555fca7d2038525d44ef184985bdc8215b
Mauricio Togneri <mauricio.togneri@gmail.com>
2020-10-19 22:51:52 +0200

    Storing data in Firestore

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,11 @@
 import 'package:Languages/api/categories/get_categories.dart';
+import 'package:Languages/json/json_category.dart';
+import 'package:Languages/json/json_entry.dart';
 import 'package:Languages/screens/home_screen.dart';
 import 'package:dafluta/dafluta.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen();
@@ -21,14 +25,57 @@ class _SplashScreenState extends State<SplashScreen> {
 
   Future _init() async {
     try {
+      await Firebase.initializeApp();
+
       final result = await GetCategories()();
 
       if (result.success) {
+        for (final JsonCategory category in result.data) {
+          final collectionReference = FirebaseFirestore.instance
+              .collection('categories')
+              .doc(category.name)
+              .collection('values');
+
+          for (final JsonEntry entry in category.values) {
+            final docReference = collectionReference.doc(entry.es);
+            final document = await docReference.get();
+
+            if (document.exists) {
+              final data = document.data();
+
+              if (data['fr'] != entry.fr) {
+                docReference.update({
+                  'fr': entry.fr,
+                });
+              }
+            } else {
+              collectionReference.doc(entry.es).set({
+                'fr': entry.fr,
+              });
+            }
+
+            /*final snapshot =
+                await reference.where('es', isEqualTo: entry.es).get();
+
+            if (snapshot.size == 1) {
+              final document = snapshot.docs[0];
+              reference.doc(document.id).update({
+                'fr': entry.fr,
+              });
+            } else {
+              reference.add({
+                'fr': entry.fr,
+              });
+            }*/
+          }
+        }
+
         Navigator.of(context).pushReplacement(HomeScren.instance(result.data));
       } else {
         _showError();
       }
     } catch (e) {
+      print(e);
       _showError();
     }
   }
