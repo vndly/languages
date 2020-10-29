1171fdc988c1cd8a0cd042ae120bd6310a51379e
Mauricio Togneri <mauricio.togneri@gmail.com>
2020-10-29 15:30:12 +0100

    Manual synch

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,10 @@
+import 'package:Languages/api/get_categories.dart';
+import 'package:Languages/dialogs/dialogs.dart';
+import 'package:Languages/json/json_category.dart';
 import 'package:Languages/json/json_expression.dart';
 import 'package:Languages/models/vocabulary.dart';
 import 'package:Languages/screens/expression_list_screen.dart';
+import 'package:Languages/storage/categories_storage.dart';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -10,9 +14,6 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<JsonExpression> duplicates = vocabulary.duplicates();
-    final List<JsonExpression> untranslated = vocabulary.untranslated();
-
     return Column(
       children: [
         const Center(
@@ -27,40 +28,106 @@ class SettingsScreen extends StatelessWidget {
             ),
           ),
         ),
-        ListTile(
-          title: Text('${duplicates.length} duplicates'),
-          trailing: const Icon(
-            Icons.arrow_forward_ios,
-            color: Colors.grey,
-            size: 12,
-          ),
-          onTap: () => _onSeeDuplicates(context, duplicates),
-        ),
+        ButtonDuplicates(vocabulary),
         const Divider(height: 0.5),
-        ListTile(
-          title: Text('${untranslated.length} untranslated'),
-          trailing: const Icon(
-            Icons.arrow_forward_ios,
-            color: Colors.grey,
-            size: 12,
-          ),
-          onTap: () => _onSeeUntranslated(context, untranslated),
-        ),
+        ButtonUntranslated(vocabulary),
+        const Divider(height: 0.5),
+        ButtonSynchronize(vocabulary),
       ],
     );
   }
+}
 
-  void _onSeeDuplicates(
+class ButtonDuplicates extends StatelessWidget {
+  final Vocabulary vocabulary;
+
+  const ButtonDuplicates(this.vocabulary);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<JsonExpression> duplicates = vocabulary.duplicates();
+
+    return ListTile(
+      title: Text('${duplicates.length} duplicates'),
+      trailing: const Icon(
+        Icons.arrow_forward_ios,
+        color: Colors.grey,
+        size: 12,
+      ),
+      onTap: () => _onSelected(context, duplicates),
+    );
+  }
+
+  void _onSelected(
     BuildContext context,
     List<JsonExpression> duplicates,
   ) =>
       Navigator.of(context)
           .push(ExpressionListScreen.instance('Duplicates', duplicates));
+}
 
-  void _onSeeUntranslated(
+class ButtonUntranslated extends StatelessWidget {
+  final Vocabulary vocabulary;
+
+  const ButtonUntranslated(this.vocabulary);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<JsonExpression> untranslated = vocabulary.untranslated();
+
+    return ListTile(
+      title: Text('${untranslated.length} untranslated'),
+      trailing: const Icon(
+        Icons.arrow_forward_ios,
+        color: Colors.grey,
+        size: 12,
+      ),
+      onTap: () => _onSelected(context, untranslated),
+    );
+  }
+
+  void _onSelected(
     BuildContext context,
     List<JsonExpression> untranslated,
   ) =>
       Navigator.of(context)
           .push(ExpressionListScreen.instance('Untranslated', untranslated));
+}
+
+class ButtonSynchronize extends StatefulWidget {
+  final Vocabulary vocabulary;
+
+  const ButtonSynchronize(this.vocabulary);
+
+  @override
+  _ButtonSynchronizeState createState() => _ButtonSynchronizeState();
+}
+
+class _ButtonSynchronizeState extends State<ButtonSynchronize> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const Text('Synchronize'),
+      onTap: () => _onSelected(context),
+    );
+  }
+
+  Future _onSelected(BuildContext context) async {
+    final dialog =
+        await Dialogs.showLoadingDialog(context, 'Downloading data...');
+
+    final result = await GetCategories()();
+
+    Navigator.of(dialog).pop();
+
+    if (result.success) {
+      setState(() {
+        final List<JsonCategory> categories = result.data;
+        widget.vocabulary.fill(categories);
+        CategoriesStorage.save(categories);
+      });
+    } else {
+      Dialogs.showErrorDialog(context, 'Error downloading data');
+    }
+  }
 }
